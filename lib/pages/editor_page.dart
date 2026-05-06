b3edafd10f03bdab02325f702118ce339d09ea85
amitprasad93651-ai <amitprasad93651@gmail.com>
2026-05-06 11:37:45 +0530

    Add placeholder editor page.

---
+++ lib/pages/editor_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditorPage extends StatelessWidget {
+  const EditorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Editor')),
+      body: const Center(
+        child: Text('Editor Screen Coming Soon'),
+      ),
+    );
+  }
+}
